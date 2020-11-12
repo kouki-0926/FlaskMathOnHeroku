44a99de569c2584b66553c5767aac4815c537826
kouki-0926 <terakou37910@icloud.com>
2020-11-12 17:45:30 +0900

    update template

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ app.register_blueprint(Math,url_prefix="/flask_math")
 app.register_blueprint(cpu,url_prefix="/flask_CPU")
 
 if __name__=="__main__":
-    app.run()
-    # app.run(host="0.0.0.0",port=5000)
+    # app.run()
+    app.run(host="0.0.0.0",port=5000)
