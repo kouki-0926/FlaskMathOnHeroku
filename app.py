919f07f36b32e4e53050f1d41411c119ac6327bb
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2021-02-15 16:59:24 +0900

    add flask_raspi

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 import sys
 sys.dont_write_bytecode = True
 
-from flask_game.views import game
-from flask_arduino.views import arduino
-from flask_CPU.views import cpu
-from flask_math.views import Math
 from main.views import main
+from flask_math.views import Math
+from flask_CPU.views import cpu
+from flask_arduino.views import arduino
+from flask_raspi.views import raspi
+from flask_game.views import game
 
 
 app = Flask(__name__)
@@ -16,8 +17,8 @@ app.register_blueprint(main)
 app.register_blueprint(Math, url_prefix="/flask_math")
 app.register_blueprint(cpu, url_prefix="/flask_CPU")
 app.register_blueprint(arduino, url_prefix="/flask_arduino")
+app.register_blueprint(raspi, url_prefix="/flask_raspi")
 app.register_blueprint(game, url_prefix="/game")
 
 if __name__ == "__main__":
-    # app.run()
     app.run(host="0.0.0.0", port=5000)
