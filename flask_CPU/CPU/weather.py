a6e499d43aca28c817092c1845aa3de025a8f42c
Terakado Koki <53304697+terakou37910@users.noreply.github.com>
2021-03-09 02:16:28 +0900

    update cpu

--- flask_CPU/CPU/weather.py
+++ flask_CPU/CPU/weather.py
@@ -2,10 +2,12 @@ import requests
 
 
 def STR(a):
-    a = a.replace("\u3000", "").replace("\n", "")
-    a = a.translate(str.maketrans(
-        {chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
-    return a
+    try:
+        b = a.replace("\u3000", "").replace("\n", "")
+        b = b.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+        return b
+    except:
+        return a
 
 
 def get_weather(pref_num):
@@ -16,15 +18,12 @@ def get_weather(pref_num):
     data["description"]["headlineText"] = STR(data["description"]["headlineText"])
     data["description"]["bodyText"] = STR(data["description"]["bodyText"])
 
-    forecasts = data["forecasts"]
-
     for i in range(3):
-        forecasts[i]["detail"]["wind"] = STR(forecasts[i]["detail"]["wind"])
-        forecasts[i]["detail"]["wave"] = STR(forecasts[i]["detail"]["wave"])
-
-    return [data, forecasts]
+        data["forecasts"][i]["detail"]["wind"] = STR(data["forecasts"][i]["detail"]["wind"])
+        data["forecasts"][i]["detail"]["wave"] = STR(data["forecasts"][i]["detail"]["wave"])
+    return data
 
 
 if(__name__ == "__main__"):
     data = get_weather("130010")
-    print(data[1][0]["detail"]["wave"])
+    print(data)
