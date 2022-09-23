abdf0e23487fc8f804ad40d8d89abd7df39767d6
Terakado Koki <terakou37910@icloud.com>
2022-09-23 21:45:42 +0900

    update js

--- flask_CPU/static_cpu/js/copy.js
+++ flask_CPU/static_cpu/js/copy.js
@@ -1,17 +1,21 @@
 function copy() {
-    const element = document.querySelector('#en_text');
-    const selection = window.getSelection();
-    const range = document.createRange();
+  const element = document.querySelector("#en_text");
+  const selection = window.getSelection();
+  const range = document.createRange();
 
-    range.selectNodeContents(element);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    const succeeded = document.execCommand('copy');
+  range.selectNodeContents(element);
+  selection.removeAllRanges();
+  selection.addRange(range);
+  const succeeded = document.execCommand("copy");
 
-    if (succeeded) {
-        alert('コピーが成功しました！');
-    } else {
-        alert('コピーが失敗しました!');
-    }
-    selection.removeAllRanges();
+  if (succeeded) {
+    alert("コピーが成功しました！");
+  } else {
+    alert("コピーが失敗しました!");
+  }
+  selection.removeAllRanges();
+}
+
+function delete_en_text() {
+  document.getElementById("en_text").value = "";
 }
