9695f2d69cb31973bccdbbbd9ab5e4126313b3f8
Terakado Koki <terakou37910@icloud.com>
2022-09-23 14:27:42 +0900

    update google translate

---
+++ flask_CPU/static_cpu/js/copy.js
@@ -0,0 +1,17 @@
+function copy() {
+    const element = document.querySelector('#ja_text');
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const succeeded = document.execCommand('copy');
+
+    if (succeeded) {
+        alert('コピーが成功しました！');
+    } else {
+        alert('コピーが失敗しました!');
+    }
+    selection.removeAllRanges();
+}
