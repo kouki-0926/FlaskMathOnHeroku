5ef29f552a01749b5bc46df753d12a72854aa8d5
Terakado Koki <terakou37910@icloud.com>
2022-09-25 19:41:38 +0900

    update files

--- flask_ticket/views.py
+++ flask_ticket/views.py
@@ -1,15 +1,22 @@
 from flask import request, redirect, url_for, render_template, flash, Blueprint, session
+from math import ceil
 
 ticket = Blueprint("ticket", __name__,
                    template_folder='templates_ticket', static_folder="static_ticket")
 
 kagoshima = [["R30911-R30915",
               "R30914",
-              "R30914"],
+              "R30914",
+              "R30915",
+              "R30916",
+              "R30911"],
              ["山陽本線鹿児島本線1枚目 東千葉->八代",
               "山陽本線鹿児島本線2枚目 門司->門司港(往復(ゆき))",
-              "山陽本線鹿児島本線3枚目 門司港->門司(往復(かえり))"],
-             ["kagoshima/kagoshima"+str(i)+".jpg" for i in range(3)]]
+              "山陽本線鹿児島本線3枚目 門司港->門司(往復(かえり))",
+              "山陽本線鹿児島本線4枚目 肥薩おれんじ鉄道 八代->川内",
+              "山陽本線鹿児島本線5枚目 川内->鹿児島",
+              "山陽本線鹿児島本線6枚目 東海道山陽新幹線自由席特急券 東京->新神戸"],
+             ["kagoshima/kagoshima"+str(i)+".jpg" for i in range(6)]]
 
 tohoku = [["R40301-R40303",
           "R40303-R40308",
@@ -38,10 +45,30 @@ Caption = kagoshima[1]+tohoku[1]+hokaido[1]
 imgNames = kagoshima[2]+tohoku[2]+hokaido[2]
 ID = range(len(Date))
 
+num = 7
+maxPage = ceil(len(ID)/num)
+
 
 @ticket.route("/")
 def index_view():
-    return render_template("index_ticket.html", ID=ID, Date=Date, Caption=Caption)
+    try:
+        page = int(request.args.get("page"))
+    except:
+        page = 0
+
+    if page < 0 or num*page > len(ID):
+        return redirect(url_for("ticket.index_view"))
+
+    if page == 0:
+        prePage = 0
+    else:
+        prePage = page-1
+    if page == maxPage-1:
+        nextPage = page
+    else:
+        nextPage = page+1
+
+    return render_template("index_ticket.html", ID=range(num), Date=Date[num*page:num*(page+1)], Caption=Caption[num*page:num*(page+1)], prePage=prePage, page=page, nextPage=nextPage)
 
 
 @ticket.route("/ticket")
