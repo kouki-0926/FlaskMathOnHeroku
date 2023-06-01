151a47dccec2adc1850fe88d061a31a9b9b05683
Terakado Koki <terakou37910@icloud.com>
2023-06-02 01:35:29 +0900

    update script

--- flask_ticket/ticket/script/eraser_exif.py
+++ flask_ticket/ticket/script/eraser_exif.py
@@ -10,11 +10,16 @@ def remove_exif_folder(folder_path):
         # ファイルの拡張子をチェックして画像ファイルか確認
         _, file_ext = os.path.splitext(file_name)
         if file_ext.lower() in ['.jpg', '.jpeg', '.png', '.gif']:
-            # 画像ファイルのパスを作成
+            # 画像読み込み
             image_path = os.path.join(folder_path, file_name)
+            image = Image.open(image_path)
+
+            # # 縦長の場合90度回転させる
+            # width, height = image.size
+            # if height > width:
+            #     image = image.rotate(90, expand=True)
 
             # Exifメタデータを削除
-            image = Image.open(image_path)
             image_without_exif = Image.new(image.mode, image.size)
             image_without_exif.putdata(list(image.getdata()))
 
