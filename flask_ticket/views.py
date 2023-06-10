4ca0194bcae09c70440e313761ea0f29da6bea53
Terakado Koki <terakou37910@icloud.com>
2023-06-11 01:15:51 +0900

    update ticket

--- flask_ticket/views.py
+++ flask_ticket/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_ticket.ticket.tokyo import tokyo
 from flask_ticket.ticket.kobe import kobe
 from flask_ticket.ticket.ise_bousou_nagano import ise, bousou, nagano
@@ -19,7 +19,20 @@ def index_view():
 
 @ticket.route("/<name>", methods=["GET"])
 def list_view(name):
-    return render_template("list_ticket.html", index_info=index_info, name=name, disp_info=globals()[name])
+    page_id = request.args.get("page_id")
+    if page_id is None:
+        page_id = 0
+    else:
+        page_id = int(page_id)
+
+    NUM = 6  # 1ページに表示するチケットの数
+    min_id = page_id * NUM
+    if (min_id < 0 or min_id > len(globals()[name])):
+        min_id = 0
+    max_id = min_id + NUM
+    if (max_id > len(globals()[name])):
+        max_id = len(globals()[name])
+    return render_template("list_ticket.html", index_info=index_info, name=name, disp_info=globals()[name], min_id=min_id, max_id=max_id, page_id=page_id)
 
 
 @ticket.route("/<name>/img<id>", methods=["GET"])
