13917335af5d9df2766b5ffbd83dc0284d4f4211
Terakado Koki <terakou37910@icloud.com>
2023-06-10 14:55:44 +0900

    add picture

--- flask_ticket/ticket/script/eraser_exif.py
+++ flask_ticket/ticket/script/eraser_exif.py
@@ -7,24 +7,29 @@ def remove_exif_folder(folder_path):
     file_list = os.listdir(folder_path)
 
     for file_name in file_list:
-        # ファイルの拡張子をチェックして画像ファイルか確認
-        _, file_ext = os.path.splitext(file_name)
-        if file_ext.lower() in ['.jpg', '.jpeg', '.png', '.gif']:
-            # 画像読み込み
-            image_path = os.path.join(folder_path, file_name)
-            image = Image.open(image_path)
+        try:
+            # ファイルの拡張子をチェックして画像ファイルか確認
+            _, file_ext = os.path.splitext(file_name)
+            if file_ext.lower() in ['.jpg', '.jpeg', '.png', '.gif']:
+                # 画像読み込み
+                image_path = os.path.join(folder_path, file_name)
+                image = Image.open(image_path)
 
-            # # 縦長の場合90度回転させる
-            # width, height = image.size
-            # if height > width:
-            #     image = image.rotate(90, expand=True)
+                # # 縦長の場合90度回転させる
+                # width, height = image.size
+                # if height > width:
+                #     image = image.rotate(90, expand=True)
 
-            # Exifメタデータを削除
-            image_without_exif = Image.new(image.mode, image.size)
-            image_without_exif.putdata(list(image.getdata()))
+                # Exifメタデータを削除
+                image_without_exif = Image.new(image.mode, image.size)
+                image_without_exif.putdata(list(image.getdata()))
 
-            # 元のファイルを上書き保存
-            image_without_exif.save(image_path)
+                # 元のファイルを上書き保存
+                image_without_exif.save(image_path)
+
+                print("Success: " + file_name)
+        except:
+            print("Error: " + file_name)
 
 
 remove_exif_folder("flask_ticket/static_ticket/images/bousou")
@@ -36,3 +41,4 @@ remove_exif_folder("flask_ticket/static_ticket/images/kobe")
 remove_exif_folder("flask_ticket/static_ticket/images/nagano")
 remove_exif_folder("flask_ticket/static_ticket/images/tohoku")
 remove_exif_folder("flask_ticket/static_ticket/images/tokyo")
+remove_exif_folder("flask_ticket/static_ticket/images/food")
