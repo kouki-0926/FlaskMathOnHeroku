b4984492cb38edb90d9a817580098ed2a2d8f47b
Terakado Koki <terakou37910@icloud.com>
2023-06-10 18:04:16 +0900

    update food

--- flask_ticket/ticket/__init__.py
+++ flask_ticket/ticket/__init__.py
@@ -5,6 +5,7 @@ from . import tohoku
 from . import ise_bousou_nagano
 from . import hokaido
 from . import hokuriku
+from . import food
 
 index_info = [["令和元年9月17日-9月21日", "東海道本線全線乗り通し", "kobe"],
               ["令和3年8月25日", "伊勢神宮参拝", "ise"],
@@ -13,4 +14,5 @@ index_info = [["令和元年9月17日-9月21日", "東海道本線全線乗り�
               ["令和4年2月12日", "長野旅行", "nagano"],
               ["令和4年3月1日-3月8日", "東北一周旅行", "tohoku"],
               ["令和4年8月30日-9月7日", "北海道一周旅行", "hokaido"],
-              ["令和5年3月29日-4月2日", "北陸旅行", "hokuriku"]]
+              ["令和5年3月29日-4月2日", "北陸旅行", "hokuriku"],
+              ["", "ご当地料理", "food"]]
