0918f1d47fc1aea86c296ecccd93bbbe1c3eabc0
Terakado Koki <terakou37910@icloud.com>
2023-11-23 22:51:54 +0900

    add jmap

---
+++ flask_ticket/static_ticket/js/jmap_param.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('#jmap').jmap({
+        width: '100%',
+        height: '100%',
+        lineColor: '#EFE4A6',
+        lineWidth: 2,
+        showTextShadow: true,
+        backgroundColor: '#6FCFDD',
+        backgroundRadius: '0.5rem',
+        backgroundPadding: '0.5rem',
+        dividerColor: '#EFE4A6',
+        showPrefectureName: true,
+        showRoundedPrefecture: true,
+        prefectureNameType: 'full',
+        prefectureBackgroundColor: '#62B34C',
+        prefectureBackgroundHoverColor: '#95A834',
+        prefectureLineColor: '#EFE4A6',
+        prefectureLineHoverColor: '#ffffff',
+        prefectureLineWidth: '2px',
+        prefectureLineGap: '0px',
+        prefectureInnerLineWidth: '1px',
+        prefectureInnerLineColor: '#EFE4A6',
+        prefectureInnerLineType: 'outset',
+        prefectureRadius: '15px',
+        prefectureLineY: '2px',
+        prefectureLineX: '2px',
+        fontSize: '0.6rem',
+        fontColor: '#fff',
+
+        onSelect: function (e, data) {
+            location.href = "/pref_id=" + data.code;
+        },
+    });
+});
