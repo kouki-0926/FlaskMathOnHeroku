567a083d9bbd7e1ff04cff8208d9f3f57b140b6f
Terakado Koki <terakou37910@icloud.com>
2024-08-24 18:55:29 +0900

    update travel

--- flask_travel/views.py
+++ flask_travel/views.py
@@ -2,64 +2,64 @@ from flask import Blueprint, render_template, request, redirect, url_for
 import requests
 
 # 切符
-from flask_ticket.ticket.tokyo import tokyo
-from flask_ticket.ticket.kobe import kobe
-from flask_ticket.ticket.ise import ise
-from flask_ticket.ticket.kagoshima import kagoshima
-from flask_ticket.ticket.bousou import bousou
-from flask_ticket.ticket.nagano import nagano
-from flask_ticket.ticket.tohoku import tohoku
-from flask_ticket.ticket.hokaido import hokaido
-from flask_ticket.ticket.hokuriku import hokuriku
-from flask_ticket.ticket.internship import internship
-from flask_ticket.ticket.shikoku import shikoku
-from flask_ticket.ticket.nara import nara
-from flask_ticket.ticket.si2023 import si2023
-from flask_ticket.ticket.sanin import sanin
-from flask_ticket.ticket import contents_ticket
+from flask_travel.travel.tokyo import tokyo
+from flask_travel.travel.kobe import kobe
+from flask_travel.travel.ise import ise
+from flask_travel.travel.kagoshima import kagoshima
+from flask_travel.travel.bousou import bousou
+from flask_travel.travel.nagano import nagano
+from flask_travel.travel.tohoku import tohoku
+from flask_travel.travel.hokaido import hokaido
+from flask_travel.travel.hokuriku import hokuriku
+from flask_travel.travel.internship import internship
+from flask_travel.travel.shikoku import shikoku
+from flask_travel.travel.nara import nara
+from flask_travel.travel.si2023 import si2023
+from flask_travel.travel.sanin import sanin
+from flask_travel.travel import contents_travel
 
 
-ticket = Blueprint("ticket", __name__, template_folder='templates_ticket', static_folder="static_ticket")
+travel = Blueprint("travel", __name__, template_folder='templates_travel', static_folder="static_travel")
 
 
-@ticket.route("/")
+@travel.route("/")
 def index_view():
-    return render_template("index_ticket.html", contents_ticket=contents_ticket)
+    return render_template("index_travel.html", contents_travel=contents_travel)
 
 
-@ticket.route("/<name>", methods=["GET"])
-def ticket_index_view(name):
+@travel.route("/<name>", methods=["GET"])
+def travel_index_view(name):
     page_id = request.args.get("page_id")
     if page_id is None:
-        return redirect(url_for('ticket.ticket_index_view', name=name, page_id=0))
+        return redirect(url_for('travel.travel_index_view', name=name, page_id=0))
     else:
         page_id = int(page_id)
 
     NUM = 6  # 1ページに表示するチケットの数
     min_id = page_id * NUM
     if (min_id < 0 or min_id > len(globals()[name])):
-        return redirect(url_for('ticket.ticket_index_view', name=name, page_id=0))
+        return redirect(url_for('travel.travel_index_view', name=name, page_id=0))
 
     max_id = min_id + NUM
     if (max_id > len(globals()[name])):
         max_id = len(globals()[name])
         page_id = int(max_id / NUM)
 
-    return render_template("ticket_index.html", contents_ticket=contents_ticket, name=name, disp_contents=globals()[name], min_id=min_id, max_id=max_id, page_id=page_id)
+    return render_template("travel_index.html", contents_travel=contents_travel, name=name, disp_contents=globals()[name], min_id=min_id, max_id=max_id, page_id=page_id)
 
 
-@ticket.route("/<name>/img<id>", methods=["GET"])
-def ticket_view(name, id):
-    return render_template("ticket.html", contents_ticket=contents_ticket, name=name, disp_contents=globals()[name], id=int(id))
+@travel.route("/<name>/img<id>", methods=["GET"])
+def travel_view(name, id):
+    return render_template("travel.html", contents_travel=contents_travel, name=name, disp_contents=globals()[name], id=int(id))
 
 
 # blog
-@ticket.route("/blog/map", methods=["GET"])
+@travel.route("/blog/map", methods=["GET"])
 def map_view():
-    return render_template("japan_map.html", contents_ticket=contents_ticket)
+    return render_template("japan_map.html", contents_travel=contents_travel)
 
 
-@ticket.route("/blog/<pref_name>", methods=["GET"])
+@travel.route("/blog/<pref_name>", methods=["GET"])
 def blog_view(pref_name):
     response = requests.get("https://raw.githubusercontent.com/kouki-0926/FlaskMathOnHeroku_Images/main/blog/image_info.json")
     image_info = response.json()
@@ -71,4 +71,4 @@ def blog_view(pref_name):
         centerCoordinates = image_info[pref_name]["centerCoordinates"]
         markers = image_info[pref_name]["markers"]
         pref_name = pref_name.split("_")[1]
-    return render_template("blog.html", contents_ticket=contents_ticket, pref_name=pref_name, centerCoordinates=centerCoordinates, markers=markers)
+    return render_template("blog.html", contents_travel=contents_travel, pref_name=pref_name, centerCoordinates=centerCoordinates, markers=markers)
