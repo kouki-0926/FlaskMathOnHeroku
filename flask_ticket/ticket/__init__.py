d5c30c43d6d54a2bfc4b2382f28757645c5176fa
Terakado Koki <terakou37910@icloud.com>
2025-03-02 15:07:16 +0900

    ファイル名の修正と内容の更新

--- flask_ticket/ticket/__init__.py
+++ flask_ticket/ticket/__init__.py
@@ -1,30 +1,31 @@
 from . import R0_Conference
 from . import R0_Tokyo
-from . import R1_Toukaido
+from . import R1_Tokaido
 from . import R3_Ise
-from . import R3_Sanyou_Kyuusyu
+from . import R3_Sanyo_Kyushu
 from . import R4_Bousou_Nagano
 from . import R4_Hokkaido
-from . import R4_Tohoku
+from . import R4_Tohoku_Ou
 from . import R5_Hokuriku
 from . import R5_Internship
 from . import R5_Nara
 from . import R5_Shikoku
 from . import R6_Kusatsu
-from . import R6_Kyuusyu
+from . import R6_Kyushu
 from . import R6_Okinawa
 from . import R6_Sanin
 from . import R7_Takayama
+from . import R7_Tohoku_Uetsu
 from . import R7_Yamanashi
 
 contents_ticket = [
     ["令和3年10月30日~", "東京", "tokyo"],
     ["令和4年12月14日~", "学会", "conference"],
-    ["令和元年09月17日~09月21日", "東海道本線", "toukaido"],
+    ["令和元年09月17日~09月21日", "東海道本線", "tokaido"],
     ["令和3年08月25日~08月25日", "伊勢神宮参拝", "ise"],
-    ["令和3年09月11日~09月17日", "山陽九州旅行", "sanyou_kyuusyu"],
+    ["令和3年09月11日~09月17日", "山陽九州旅行", "sanyo_kyushu"],
     ["令和4年02月11日~02月12日", "週末パス(房総・長野)", "bousou_nagano"],
-    ["令和4年03月01日~03月08日", "東北一周旅行", "tohoku"],
+    ["令和4年03月01日~03月08日", "東北一周旅行", "tohoku_ou"],
     ["令和4年08月30日~09月07日", "北海道一周旅行", "hokkaido"],
     ["令和5年03月29日~04月02日", "北陸旅行", "hokuriku"],
     ["令和5年08月09日~09月20日", "夏インターンシップ", "internship"],
@@ -32,10 +33,12 @@ contents_ticket = [
     ["令和5年11月21日~11月22日", "奈良旅行", "nara"],
     ["令和6年03月01日~03月04日", "山陰旅行", "sanin"],
     ["令和6年09月08日~09月09日", "草津温泉", "kusatsu"],
-    ["令和6年10月15日~10月19日", "九州旅行", "kyuusyu"],
+    ["令和6年10月15日~10月19日", "九州旅行", "kyushu"],
     ["令和6年11月05日~11月07日", "沖縄旅行", "okinawa"],
     ["令和7年02月08日~02月08日", "山梨旅行", "yamanashi"],
     ["令和7年02月15日~02月17日", "高山本線", "takayama"],
+    ["令和7年02月25日~02月26日", "東北一周旅行", "tohoku_uetsu"],
     ["", "旅行写真", "picture/map"],
     ["", "駅名標", "station"],
+    ["", "駅舎", "station2"],
 ]
