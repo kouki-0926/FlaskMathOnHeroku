95ab259e06d2212f3974a685f954a67751dcf1e2
Terakado Koki <terakou37910@icloud.com>
2025-05-25 21:54:41 +0900

    update

--- flask_ticket/ticket/R1_Tokaido.py
+++ flask_ticket/ticket/R1_Tokaido.py
@@ -29,15 +29,15 @@ tokaido_images = [
 ]
 
 tokaido_stations = [
-    Chiba,         Tokyo,           Ofuna,              Enoshima,        Hase,
-    Kamakura,      Ofuna,           Odawara,            Nebukawa,        Atami,
-    Mishima,       Numazu,          Shimizu,            Shizuoka,        Bentenjima,
-    Toyohashi,     Nagoya,          Fushimi,            Sakae,           Nagoya_Castle,
-    Sakae,         Kamimaezu,       Atsuta_Jingu_Nishi, Kamimaezu,       Osu_Kannon,
-    Fushimi,       Nagoya,          Ogaki,
-    Maibara,       Hikone,          Kyoto,             Inari,           Fushimi_Inari,
-    Shimizu_Gojyo, Higashiyama,     Nijyo_Castle,      Tenjingawa,      Katabiranotsuji,
-    Arashiyama,    Katabiranotsuji, Kitano_Hakubaicho, Katabiranotsuji, Tenjingawa, 
-    Shijo_Omiya,   Karasuma,        Kyoto,             Shin_Osaka,      Osaka,
-    Kobe,          Osaka,           Shin_Osaka,        Tokyo,           Chiba
+    Chiba,             Tokyo,           Ofuna,              Enoshima,      Hase,
+    Kamakura,          Ofuna,           Odawara,            Nebukawa,      Atami,
+    Mishima,           Numazu,          Shimizu,            Shizuoka,      Bentenjima,
+    Toyohashi,         Nagoya,          Fushimi,            Sakae,         Nagoya_Castle,
+    Sakae,             Kamimaezu,       Atsuta_Jingu_Nishi, Kamimaezu,     Osu_Kannon,
+    Fushimi,           Nagoya,          Ogaki,              Maibara,       Hikone,
+    Kyoto,             Inari,           Fushimi_Inari,      Shimizu_Gojyo, Higashiyama,
+    Nijyo_Castle,      Tenjingawa,      Katabiranotsuji,    Arashiyama,    Katabiranotsuji,
+    Kitano_Hakubaicho, Katabiranotsuji, Tenjingawa,         Shijo_Omiya,   Karasuma,
+    Kyoto,             Shin_Osaka,      Osaka,              Kobe,          Osaka,
+    Shin_Osaka,        Tokyo,           Chiba
 ]
