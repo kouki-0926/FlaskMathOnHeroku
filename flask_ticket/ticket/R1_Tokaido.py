f74bab73f4052003b7d563a35a50a79838be2a8f
Terakado Koki <terakou37910@icloud.com>
2025-05-25 23:02:00 +0900

    update

--- flask_ticket/ticket/R1_Tokaido.py
+++ flask_ticket/ticket/R1_Tokaido.py
@@ -40,8 +40,8 @@ tokaido_stations = [
     Kitano_Hakubaicho, Katabiranotsuji, Tenjingawa,         Shijo_Omiya,       Karasuma,
     Kyoto,             Shin_Osaka,      Osaka,              Umeda,             Shinsaibashi,
     Namba,             Dobutsuen_Mae,   Tennoji,            Tanimachi_4_Chome, Temmabashi,
-    Higashi_Umeda,     Osaka,
-    Kobe,              Osaka,           Shin_Osaka,         Kyoto,         Maibara,
+    Higashi_Umeda,     Osaka,           Kobe,               Sannomiya,     ,
+    Osaka,           Shin_Osaka,         Kyoto,         Maibara,
     Nagoya,            Toyohashi,       Shizuoka,           Mishima,       Atami,
     Odawara,           Shinagawa,       Tokyo,              Chiba
 ]
