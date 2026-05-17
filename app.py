eb9d60c852e6749b4a47b69c13e60675573c7ce3
Terakado Koki <terakou37910@icloud.com>
2026-05-17 22:02:09 +0900

    update app.py

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ app.config.from_object("config")
 
 from main.views import main
 from flask_math.views import Math
-from flask_CPU.views import cpu
+from flask_tools.views import tools
 from flask_game.views import game
 from flask_travel.views import travel
 
 app.register_blueprint(main)
 app.register_blueprint(Math, url_prefix="/math")
-app.register_blueprint(cpu, url_prefix="/CPU")
+app.register_blueprint(tools, url_prefix="/tools")
 app.register_blueprint(game, url_prefix="/game")
 app.register_blueprint(travel, url_prefix="/travel")
 
